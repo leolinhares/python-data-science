84b5dcbfa6e27fa67c8746e18eb72cb0894d2ce1
Leonardo Linhares <slinharesleo@gmail.com>
2016-10-24 20:16:27 -0300

    added linear regression from sklearn

--- regression/linearRegression.py
+++ regression/linearRegression.py
@@ -2,6 +2,7 @@ import numpy as np
 import math
 import scipy.stats as ss
 from bokeh.plotting import figure, output_file, show
+from sklearn import linear_model
 
 
 # load data
@@ -13,13 +14,21 @@ data = ss.zscore(data)
 # randomize rows and split features from labels
 ndata = np.random.permutation(data)
 features = ndata.shape[1]
-trainingFeatures = ndata[:, 0:features - 1]
-trainingLabels = ndata[:, -1]
+nt = int(math.floor(len(ndata) * 0.7))
+trainingFeatures = ndata[0:nt, 0:features - 1]
+trainingLabels = ndata[0:nt, -1]
+testFeatures = ndata[nt:, 0:features - 1]
+testLabels = ndata[nt:, -1]
 
 # add array of ones to the matrix of training features
 rows = len(trainingFeatures)
 arrayOfOnes = np.ones(rows)
 trainingFeatures = np.c_[arrayOfOnes, trainingFeatures]
+
+# add array of ones to the matrix of test features
+rows = len(testFeatures)
+arrayOfOnes = np.ones(rows)
+testFeatures = np.c_[arrayOfOnes, testFeatures]
 
 # create the initial theta array with zeros
 t = np.zeros(features)
@@ -38,19 +47,34 @@ def gradient_descent(X, y, theta, alpha, iterations):
     for i in range(iterations):
         error = y - X.dot(theta)
         for j in range(parameters):
-            temp[j] = theta[j] + (alpha/size)*(error).dot(X[:,j])
+            temp[j] = theta[j] + (alpha / size) * error.dot(X[:, j])
         theta = temp
-        cost[i] = cost_function(trainingFeatures,trainingLabels, theta)
-    return theta,cost
+        cost[i] = cost_function(trainingFeatures, trainingLabels, theta)
+    return theta, cost
 
-x,cost = gradient_descent(trainingFeatures, trainingLabels, t, 0.01, 1000)
 
-print x
-print cost
-print cost_function(trainingFeatures,trainingLabels,x)
+theta, cost = gradient_descent(trainingFeatures, trainingLabels, t, 0.01, 1000)
+
+
+def predict(X, w):
+    return X.dot(w)
+
+
+print theta
+# print cost_function(trainingFeatures, trainingLabels, theta)
+# a = predict(testFeatures, theta)
+# b = testLabels
+# print zip(a,b)
+
+regr = linear_model.LinearRegression()
+regr.fit(trainingFeatures, trainingLabels)
+print("Mean squared error: %f"
+      % np.mean((regr.predict(testFeatures) - testLabels) ** 2))
+
+print("Mean squared error 2: %f"
+      % np.mean((predict(testFeatures, theta) - testLabels) ** 2))
 
 it = np.arange(1000)
 p = figure(x_axis_label='Iterations', y_axis_label='Cost')
 p.line(it, cost, line_width=2)
 show(p)
-
